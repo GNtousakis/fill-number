0d2ca4390aa1d97dfeac57c5ff85514d9d97f844
GNtousakis <greco1996@gmail.com>
2021-01-18 14:28:39 +0200

    Change NAN_METHOD

--- src/FillNumber.cpp
+++ src/FillNumber.cpp
@@ -11,26 +11,26 @@ using v8::String;
 using v8::Value;
 using v8::Handle;
 
-void FillNumber(const FunctionCallbackInfo<Value>& args){
-  Isolate* isolate = args.GetIsolate();
+NAN_METHOD(FillNumber) {
+  Isolate* isolate = info.GetIsolate();
   
   /* Check arguments */
-  if ((args.Length() != 2 && args.Length() != 3) || !args[0]->IsNumber() || !args[1]->IsNumber()) {
+  if ((info.Length() != 2 && info.Length() != 3) || !info[0]->IsNumber() || !info[1]->IsNumber()) {
     
     isolate->ThrowException(Exception::TypeError(
         String::NewFromUtf8(isolate, "You need to pass the correct input")));
     return;
   }
   
-  int zeros = args[0]->NumberValue();
-  int number = args[1]->NumberValue();
+  int zeros = info[0]->NumberValue();
+  int number = info[1]->NumberValue();
   std::ostringstream stream;
   
-  if (args.Length() == 2) {
+  if (info.Length() == 2) {
     /* Fill the number with the right padding */
     stream << std::setfill('0') << std::setw(zeros) << number;
   } else {
-    String::Utf8Value tmp(args[2]->ToString());
+    String::Utf8Value tmp(info[2]->ToString());
     std::string str = std::string(*tmp);
     stream << std::setfill(str[0]) << std::setw(zeros) << number;
   }
@@ -38,11 +38,11 @@ void FillNumber(const FunctionCallbackInfo<Value>& args){
   /* Revert back to String and return to Javascript */
   std::string str = stream.str(); 
   v8::Local<v8::String> v8String = v8::String::NewFromUtf8(isolate, str.c_str(), v8::String::kNormalString);
-  args.GetReturnValue().Set(v8String);
+  info.GetReturnValue().Set(v8String);
 }
 
-void Init(Handle<Object> exports) {
-  NODE_SET_METHOD(exports, "FillNumber", FillNumber);
+NAN_MODULE_INIT(Init) {
+  NAN_EXPORT( target, FillNumber);
 }
 
 NODE_MODULE(FillNumber, Init)
